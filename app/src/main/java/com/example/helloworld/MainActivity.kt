4d161b4ac6450cce2953a09eb7ce61ccf8fcb2ea
1902667-JacksonWong <1902667@sit.singaporetech.edu.sg>
2021-01-22 13:51:28 +0800

    integrate CI and slack notification

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+private const val TAG = "MyActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "Hello World")
     }
+
 }
+
 //testing trololol
